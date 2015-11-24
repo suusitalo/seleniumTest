5ed418ff4afc7553f0c74be68139cae193470323
Sami Uusitalo <suusitalo76@gmail.com>
2015-11-24 23:12:21 +0200

    removed unused import
    removed unused import

--- src/test/java/com/sami/seleniumTest/webTest.java
+++ src/test/java/com/sami/seleniumTest/webTest.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
